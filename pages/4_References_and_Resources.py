583ed8d530e1a2b04d3d05764471ef3d4eb21064
Edward Ayoub <114652033+edwardayoub@users.noreply.github.com>
2025-01-14 15:14:49 -0600

    Create 4_References_and_Resources.py

---
+++ pages/4_References_and_Resources.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.title("References & Resources")
+st.write("""
+Here you'll find a curated list of literature, documentation, and helpful links:
+""")
+
+st.markdown("### Key Literature")
+st.write("""
+- [Formation of Tertiary Lymphoid Structures in Cancer (Paper Placeholder)](#)
+- [Relevant Cell paper (2020)](https://www.cell.com/cell/fulltext/S0092-8674(20)30870-9)
+- [MD Anderson Official Website](https://www.mdanderson.org/)
+- Additional publications (placeholder)
+""")
+
+st.markdown("### Documentation & Tutorials")
+st.write("""
+- [maxfuse GitHub Repo](#)
+- [CODEX Data Processing Pipeline (Placeholder)](#)
+""")
